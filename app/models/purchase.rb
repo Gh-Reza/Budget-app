6c553610a5081affe8868240bb66091a109698e8
Reza Merzaie <ghrezam97@gmail.com>
2023-11-23 20:57:17 +0430

    Implement get_total_purchases and fix some bugs

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,6 @@
 class Purchase < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :purchase_categories
   has_many :categories, through: :purchase_categories
 
   validates :name, presence: true
