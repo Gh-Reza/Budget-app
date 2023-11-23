260e1be2c7800739e348829b6675b8b313f714f8
Reza Merzaie <ghrezam97@gmail.com>
2023-11-23 09:26:10 +0430

    Create Purchase modle

--- db/migrate/20231123044726_create_categories.rb
+++ db/migrate/20231123044726_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.1]
     create_table :categories do |t|
       t.string :name
       t.string :icon
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
